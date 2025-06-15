8851f1f6e746bdfed0f71ee0359c4e4bb8685ca8
holeshit1234 <hominhdund@gmail.com>
2025-06-15 22:21:31 +0700

    Create: Layout Admin

---
+++ src/layouts/AdminLayout/Header/SidebarHeader/MenuItems.tsx
@@ -0,0 +1,17 @@
+import { Dashboard as DashboardIcon } from "@mui/icons-material";
+
+import config from "../../../../configs";
+import { type MenuItemLayout } from "../../../../types/menu";
+
+export const menuItems: MenuItemLayout[] = [
+  {
+    role: "Admin",
+    menu: [
+      {
+        icon: <DashboardIcon />,
+        label: "Dashboard",
+        path: config.adminRoutes.dashboard,
+      },
+    ],
+  },
+];
