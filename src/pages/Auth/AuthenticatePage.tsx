a44368d15c7a2c57ce4b63b8396ef92578aa2dcc
holeshit1234 <hominhdund@gmail.com>
2025-06-21 20:46:08 +0700

    change the title

--- src/pages/Auth/AuthenticatePage.tsx
+++ src/pages/Auth/AuthenticatePage.tsx
@@ -1,9 +1,15 @@
 import React from "react";
 import Login from "../../containers/authenticate/Login";
+import { Helmet } from "react-helmet";
 
 const AuthenticatePage: React.FC = () => {
   return (
     <>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>HypeCat | Đăng Nhập</title>
+        <meta name="description" content="Helmet application" />
+      </Helmet>
       <Login />
     </>
   );
